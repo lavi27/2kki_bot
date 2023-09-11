6b927184107e64303df73d822ffa2e5d1018abc1
lavi27 <62757958+lavi27@users.noreply.github.com>
2023-09-11 16:05:06 +0900

    d

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,16 @@
-import { Client, GatewayIntentBits } from 'discord.js';
+import { Client, Events, GatewayIntentBits } from 'discord.js';
 import { BOT_TOKEN } from '../config.json';
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on("messageCreate", (message) => {
+  if (message.content === "<@1023320497469005938>") {
+    message.reply("Hey!")
+  } 
 });
 
 client.on('interactionCreate', async interaction => {
