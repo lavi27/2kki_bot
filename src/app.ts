40036b0728660d95a8b7ace28ca95c755c86b892
lavi27 <62757958+lavi27@users.noreply.github.com>
2023-09-11 06:52:56 +0000

    ㅂ

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,18 @@
-console.log('asd');
+import { Client, GatewayIntentBits } from 'discord.js';
+import { BOT_TOKEN } from '../config.json';
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName === 'ping') {
+    await interaction.reply('Pong!');
+  }
+});
+
+client.login(BOT_TOKEN);
